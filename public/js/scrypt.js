1e0051f8b25e962d42c4a7296f116369443238a6
Aragi <aldairmatum@gmail.com>
2023-05-08 21:19:52 -0600

    Initial commit

---
+++ public/js/scrypt.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("Modal-1");
+var btn = document.getElementById("AbrirModal");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
